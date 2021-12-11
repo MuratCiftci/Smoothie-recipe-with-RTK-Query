169ca8e4dbcbe47d4eb067aaca70b394e075a629
Murat Ciftci <mrt.cftci.20@gmail.com>
2021-12-11 19:25:11 +0300

    changing create-react-app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Navbar } from './components/Navbar/Navbar'
+import { Routes, Route, Link } from "react-router-dom";
+import Hero from './components/Hero/Hero';
+import Recipes from './components/Recipes/Recipes';
 
-function App() {
+export  const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <Recipes title='Popular' />
+      <Recipes title='Latest' />
     </div>
-  );
+  )
 }
 
-export default App;
