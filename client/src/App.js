812d13c9b36a7a9167c74430868d6725d6c175ef
Murat Ciftci <mrt.cftci.20@gmail.com>
2021-12-25 15:55:02 +0300

    style: height problem fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Routes, Route,  BrowserRouter } from "react-router-dom";
-
+import "./index.css";
 import Home from "./pages/Home/Home";
 import RecipeDetailPage from "./pages/RecipeDetailPage/RecipeDetailPage";
 import Recipes from "./pages/AllRecipes/Recipes";
@@ -9,10 +9,13 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Write from "./pages/writeRecipe/Write";
 
+
 export const App = () => {
+
   return (
     <BrowserRouter>
       <Navbar />
+      <div className="content">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<Recipes />} />
@@ -21,6 +24,7 @@ export const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/write" element={<Write />} />
       </Routes>
+      </div>
     </BrowserRouter>
   );
 };
