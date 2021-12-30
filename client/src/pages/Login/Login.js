35c51d4f6294a9ce5f6f9889835b9287b7d360bf
Murat Ciftci <mrt.cftci.20@gmail.com>
2021-12-31 00:30:26 +0300

    feat: user authentication

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,17 +1,126 @@
-import styles from "./Login.module.scss";
+import { useEffect, useState } from "react";
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+  Button,
+  Divider,
+  Center,
+  Box,
+  useToast,
+  FormControl,
+} from "@chakra-ui/react";
 
-export default function Login() {
+import { useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../../app/features/auth/authApi";
+import { useAuth } from "../../hooks/useAuth";
+function PasswordInput({ name, onChange }) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
   return (
-    <div className={styles.login}>
-      <span className={styles.loginTitle}>Login</span>
-      <form className={styles.loginForm}>
-        <label>Email</label>
-        <input className={styles.loginInput} type="text" placeholder="Enter your email..." />
-        <label>Password</label>
-        <input className={styles.loginInput} type="password" placeholder="Enter your password..." />
-        <button className={styles.loginButton}>Login</button>
-      </form>
-        <button className={styles.loginRegisterButton}>Register</button>
-    </div>
+    <InputGroup size="md">
+      <Input
+        pr="4.5rem"
+        type={show ? "text" : "password"}
+        placeholder="Enter password"
+        name={name}
+        onChange={onChange}
+      />
+      <InputRightElement width="4.5rem">
+        <Button h="1.75rem" size="sm" onClick={handleClick}>
+          {show ? "Hide" : "Show"}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
   );
 }
+
+export const Login = () => {
+  const navigate = useNavigate();
+  const {user} = useAuth();
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  });
+  const toast = useToast();
+
+  const [formState, setFormState] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const handleChange = (e) => {
+    setFormState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(formState).unwrap();
+      // Being that the result is handled in extraReducers in authSlice,
+      // we know that we're authenticated after this, so the user
+      // and token will be present in the store
+      navigate("/");
+    } catch (err) {
+      toast({
+        status: "error",
+        title: "Error",
+        description: "Invalid username or password",
+        isClosable: true,
+      });
+    }
+  };
+  console.log(formState);
+  return (
+    <Center width="100%">
+      <Box
+        p={8}
+        mt={10}
+        maxWidth="500px"
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow="lg"
+      >
+        <form onSubmit={handleSubmit}>
+          <VStack spacing="4">
+            <FormControl isRequired>
+              <InputGroup>
+                <Input
+                  onChange={handleChange}
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl isRequired>
+              <InputGroup>
+                <PasswordInput onChange={handleChange} name="password" />
+              </InputGroup>
+            </FormControl>
+            <Button
+              isFullWidth
+              color="white"
+              type="submit"
+              bg="cyan.600"
+              _hover={{ bg: "cyan.800" }}
+              isLoading={isLoading}
+            >
+              Login
+            </Button>
+            <Divider />
+          </VStack>
+        </form>
+      </Box>
+    </Center>
+  );
+};
+
+export default Login;
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,19 +1,129 @@
-import styles from "./Register.module.scss"
+import { useState } from "react";
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+  Button,
+  Divider,
+  Center,
+  Box,
+  useToast,
+  FormControl,
+} from "@chakra-ui/react";
 
-export default function Register() {
-    return (
-        <div className={styles.register}>
-      <span className={styles.registerTitle}>Register</span>
-      <form className={styles.registerForm}>
-        <label>Username</label>
-        <input className={styles.registerInput} type="text" placeholder="Enter your username..." />
-        <label>Email</label>
-        <input className={styles.registerInput} type="text" placeholder="Enter your email..." />
-        <label>Password</label>
-        <input className={styles.registerInput} type="password" placeholder="Enter your password..." />
-        <button className={styles.registerButton}>Register</button>
-      </form>
-        <button className={styles.registerLoginButton}>Login</button>
-    </div>
-    )
+import { useNavigate } from "react-router-dom";
+
+import { useRegisterMutation } from "../../app/features/auth/authApi";
+import ErrorMessage from "../../components/Error";
+
+function PasswordInput({ name, onChange }) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <InputGroup size="md">
+      <Input
+        pr="4.5rem"
+        type={show ? "text" : "password"}
+        placeholder="Enter password"
+        name={name}
+        onChange={onChange}
+      />
+      <InputRightElement width="4.5rem">
+        <Button h="1.75rem" size="sm" onClick={handleClick}>
+          {show ? "Hide" : "Show"}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
 }
+
+export const Register = () => {
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [register, { isLoading }] = useRegisterMutation();
+  const [errors, setErrors] =useState([]);
+  console.log(errors);
+  const handleChange = (e) => {
+    setFormState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await register(formState).unwrap();
+      navigate("/login");
+    } catch (err) {
+      setErrors(err.data.errors)
+    }
+  };
+  console.log(formState);
+  return (
+    <Center width="100%">
+      <Box
+        p={8}
+        mt={10}
+        maxWidth="500px"
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow="lg"
+      >
+        <form onSubmit={handleSubmit}>
+          {errors && errors.map(e => (
+            <ErrorMessage message={e.msg} />
+          ))}
+          <VStack spacing={4}>
+            <FormControl isRequired>
+              <InputGroup>
+                <Input
+                  onChange={handleChange}
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl isRequired>
+              <InputGroup>
+                <Input
+                  onChange={handleChange}
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl isRequired>
+              <InputGroup>
+                <PasswordInput onChange={handleChange} name="password" />
+              </InputGroup>
+            </FormControl>
+            <Button
+              isFullWidth
+              color="white"
+              type="submit"
+              bg="cyan.600"
+              _hover={{ bg: "cyan.800" }}
+              isLoading={isLoading}
+            >
+              Register
+            </Button>
+            <Divider />
+          </VStack>
+        </form>
+      </Box>
+    </Center>
+  );
+};
+
+export default Register;
