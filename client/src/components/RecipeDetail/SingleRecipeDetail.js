a8c1f6bd6dce62fafca03ead6d35ca89bb912de4
Murat Ciftci <mrt.cftci.20@gmail.com>
2021-12-31 15:35:54 +0300

    feat: recipe details

--- client/src/components/RecipeDetail/SingleRecipeDetail.js
+++ client/src/components/RecipeDetail/SingleRecipeDetail.js
@@ -1,34 +1,41 @@
 import React from "react";
 import styles from "./SingleRecipeDetail.module.scss";
-const RecipeDetail = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <div className={styles.recipeTitle}>Dummy Recipe Title</div>
-        <div className={styles.content}>
-          <img
-            className={styles.recipeImage}
-            src={"./recipedetail.jpg"}
-            alt="recipe"
-          />
-          <div className={styles.ingredientContainer}>
-            <h3 className={styles.ingTitle}>Ingredient</h3>
-            <section className={styles.ingredientsList}>
-              <li className={styles.ingredient}>Banana</li>
-              <li className={styles.ingredient}>Chia seeds</li>
-              <li className={styles.ingredient}>Peanut Butter</li>
-              <li className={styles.ingredient}>Milk</li>
-              <li className={styles.ingredient}>Oatmeal</li>
-            </section>
+
+const RecipeDetail = ({ recipe, isLoading }) => {
+  const PF = "http://localhost:8000/images/";
+
+  if (isLoading) return <div>Loading..</div>;
+
+  if (recipe) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.recipeTitle}>{recipe.title}</div>
+          <div className={styles.content}>
+            <img
+              className={styles.recipeImage}
+              src={PF + recipe.photo}
+              alt="recipe"
+            />
+              <div className={styles.ingredientContainer}>
+                <h3 className={styles.ingTitle}>Ingredients</h3>
+                <section className={styles.ingredientsList}>
+                  {recipe.ingredients.map((ingredient, index) => (
+                    <li key={index} className={styles.ingredient}>
+                      {ingredient}
+                    </li>
+                  ))}
+                </section>
+              </div>
+              
           </div>
-        </div>
-        <div className={styles.description}>
-          Combine bananas, pineapple, almond butter, and chia seeds in a
-          blender; add water. Blend until smooth.
+          <div className={styles.description}>
+            <p className={styles.descTitle}>Directions</p>
+            {recipe.desc}</div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default RecipeDetail;
--- client/src/pages/RecipeDetailPage/RecipeDetailPage.js
+++ client/src/pages/RecipeDetailPage/RecipeDetailPage.js
@@ -1,10 +1,14 @@
-import React from 'react'
 import SingleRecipeDetail from '../../components/RecipeDetail/SingleRecipeDetail'
-
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
+import {useGetRecipeQuery} from '../../app/features/recipe/recipeApi'
 const RecipeDetailPage = () => {
+  const { id } = useParams();
+  const {data: recipe, isLoading} = useGetRecipeQuery(id);
+
     return (
         <div>
-            <SingleRecipeDetail />
+            <SingleRecipeDetail  isLoading={isLoading} recipe={recipe} />
         </div>
     )
 }
