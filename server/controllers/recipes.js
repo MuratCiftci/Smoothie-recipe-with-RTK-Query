f6ac95bef99c101e2a510e9421b5677667709503
Murat Ciftci <mciftci174@posta.pau.edu.tr>
2022-01-02 16:19:11 +0300

    fix: typo

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -42,10 +42,10 @@ exports.updateRecipe = async (req, res) => {
   }
 };
 
-exports.delete = async (req, res) => {
+exports.deleteRecipe = async (req, res) => {
   try {
     const recipe = await Recipe.findById(req.params.id);
-    if (req.user.username === recipe.usename) {
+    if (req.user.username === recipe.username) {
       try {
         await recipe.delete();
         res.status(200).json("Recipe has been deleted");
